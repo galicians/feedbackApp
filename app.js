2ede97b41742e567e5a87bcac0aa1bc1ef7faf10
galicians <galicians@gmail.com>
2014-11-15 20:37:50 +0000

    body parser added to express

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
-var io = require('socket.io')(server)
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser')
 
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req,res) {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 })
 
 
 function init() {
 
   server.listen(port, function() {
-    console.log("Server is listening on port " + port)
+    console.log("Server is listening on port " + port);
   })
 }
 
 
-init()
+init();
