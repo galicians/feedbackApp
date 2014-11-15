b04690e00964ff5b4a7051105bcbed1b5d12b7de
galicians <galicians@gmail.com>
2014-11-15 20:13:33 +0000

    server running, packages installed

---
+++ README.md
@@ -0,0 +1,14 @@
+###feedbackApp
+
+
+
+feedback app to capture sms insights and save in db
+
+using the following tools:
+
+deploying to nodejitsu
+coachdb
+node
+angular
+express
+twilio
---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req,res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+
+function init() {
+
+  server.listen(5000, function() {
+    console.log("the server is listening on port 5000")
+  })
+}
+
+
+init()
